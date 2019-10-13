c0b224acb258ec4e336e04573eb7f5db81c49c19
morrys <lorenzo.digiacomo85@gmail.com>
2019-10-13 20:29:18 +0200

    update version netinfo, feat(netinfo ssr)

--- packages/netinfo/src/NetInfo.ts
+++ packages/netinfo/src/NetInfo.ts
@@ -10,29 +10,28 @@
 import ExecutionEnvironment from 'fbjs/lib/ExecutionEnvironment';
 import * as findIndex from 'array-find-index';
 
-const connection = ExecutionEnvironment.canUseDOM && (
-  (window.navigator as any).connection ||
-  (window.navigator as any).mozConnection ||
-  (window.navigator as any).webkitConnection);
+const connection =
+    ExecutionEnvironment.canUseDOM &&
+    ((window.navigator as any).connection || (window.navigator as any).mozConnection || (window.navigator as any).webkitConnection);
 
 // Prevent the underlying event handlers from leaking and include additional
 // properties available in browsers
 const getConnectionInfoObject = () => {
-  const result = {
-    effectiveType: 'unknown',
-    type: 'unknown',
-    isConnected: !ExecutionEnvironment.canUseDOM || window.navigator.onLine
-  };
-  if (!connection) {
-    return result;
-  }
-  for (const prop in connection) {
-    const value = connection[prop];
-    if (typeof value !== 'function' && value != null) {
-      result[prop] = value;
+    const result = {
+        effectiveType: 'unknown',
+        type: 'unknown',
+        isConnected: !ExecutionEnvironment.canUseDOM || window.navigator.onLine,
+    };
+    if (!connection) {
+        return result;
     }
-  }
-  return result;
+    for (const prop in connection) {
+        const value = connection[prop];
+        if (typeof value !== 'function' && value != null) {
+            result[prop] = value;
+        }
+    }
+    return result;
 };
 
 const netInfoListeners = [];
@@ -43,77 +42,75 @@ const connectionListeners = [];
  * Network Connection API: https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation
  */
 const NetInfo = {
-  addEventListener(handler: Function): { remove: () => void } {
-    if (!connection) {
-      console.error(
-        'Network Connection API is not supported. Not listening for connection type changes.'
-      );
-      return {
-        remove: () => {}
-      };
-    }
-
-    const wrappedHandler = () => handler(getConnectionInfoObject());
-    netInfoListeners.push([handler, wrappedHandler]);
-    connection.addEventListener('change', wrappedHandler);
-    return {
-      remove: () => NetInfo.removeEventListener(handler)
-    };
-  },
-
-  removeEventListener(handler: Function): void {
-
-    const listenerIndex = findIndex(netInfoListeners, pair => pair[0] === handler);
-    const [, wrappedHandler] = netInfoListeners[listenerIndex];
-    connection.removeEventListener('change', wrappedHandler);
-    netInfoListeners.splice(listenerIndex, 1);
-  },
-
-  fetch(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      try {
-        resolve(getConnectionInfoObject());
-      } catch (err) {
-        resolve('unknown');
-      }
-    });
-  },
-  isConnected: {
-    addEventListener(type: string, handler: Function): { remove: () => void } {
-
-      const onlineCallback = () => handler(true);
-      const offlineCallback = () => handler(false);
-      connectionListeners.push([handler, onlineCallback, offlineCallback]);
-
-      window.addEventListener('online', onlineCallback, false);
-      window.addEventListener('offline', offlineCallback, false);
-
-      return {
-        remove: () => NetInfo.isConnected.removeEventListener('connectionChange', handler)
-      };
-    },
-
-    removeEventListener(type: string, handler: Function): void {
-      const listenerIndex = findIndex(connectionListeners, pair => pair[0] === handler);
-      const [, onlineCallback, offlineCallback] = connectionListeners[listenerIndex];
-
-      window.removeEventListener('online', onlineCallback, false);
-      window.removeEventListener('offline', offlineCallback, false);
-
-      connectionListeners.splice(listenerIndex, 1);
-    },
-
-    fetch(): Promise<boolean> {
-      return new Promise((resolve, reject) => {
-        try {
-          resolve(window.navigator.onLine);
-        } catch (err) {
-          resolve(true);
+    addEventListener(handler: Function): { remove: () => void } {
+        if (!connection) {
+            console.error('Network Connection API is not supported. Not listening for connection type changes.');
+            return {
+                remove: () => {},
+            };
         }
-      });
-    }
-  }
-};
 
+        const wrappedHandler = () => handler(getConnectionInfoObject());
+        netInfoListeners.push([handler, wrappedHandler]);
+        connection.addEventListener('change', wrappedHandler);
+        return {
+            remove: () => NetInfo.removeEventListener(handler),
+        };
+    },
+
+    removeEventListener(handler: Function): void {
+        const listenerIndex = findIndex(netInfoListeners, (pair) => pair[0] === handler);
+        const [, wrappedHandler] = netInfoListeners[listenerIndex];
+        connection.removeEventListener('change', wrappedHandler);
+        netInfoListeners.splice(listenerIndex, 1);
+    },
+
+    fetch(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                resolve(getConnectionInfoObject());
+            } catch (err) {
+                resolve('unknown');
+            }
+        });
+    },
+    isConnected: {
+        addEventListener(type: string, handler: Function): { remove: () => void } {
+            const onlineCallback = () => handler(true);
+            const offlineCallback = () => handler(false);
+            connectionListeners.push([handler, onlineCallback, offlineCallback]);
+
+            if (ExecutionEnvironment.canUseDOM) {
+                window.addEventListener('online', onlineCallback, false);
+                window.addEventListener('offline', offlineCallback, false);
+            }
+
+            return {
+                remove: () => NetInfo.isConnected.removeEventListener('connectionChange', handler),
+            };
+        },
+
+        removeEventListener(type: string, handler: Function): void {
+            const listenerIndex = findIndex(connectionListeners, (pair) => pair[0] === handler);
+            const [, onlineCallback, offlineCallback] = connectionListeners[listenerIndex];
+
+            if (ExecutionEnvironment.canUseDOM) {
+                window.removeEventListener('online', onlineCallback, false);
+                window.removeEventListener('offline', offlineCallback, false);
+            }
+            connectionListeners.splice(listenerIndex, 1);
+        },
+
+        fetch(): Promise<boolean> {
+            return new Promise((resolve, reject) => {
+                try {
+                    resolve(!ExecutionEnvironment.canUseDOM || window.navigator.onLine);
+                } catch (err) {
+                    resolve(true);
+                }
+            });
+        },
+    },
+};
 
 export default NetInfo;
