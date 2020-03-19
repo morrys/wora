5c886512a333eeafb1726ebccba5e2eaa8ed5492
morrys <lorenzo.digiacomo85@gmail.com>
2020-03-19 20:08:18 +0100

    update typescript & added tests relay-offline

---
+++ packages/relay-offline/__tests__/RelayModernEnvironment-ApplyUpdate-test.ts
@@ -0,0 +1,210 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ * @emails oncall+relay
+ */
+
+// flowlint ambiguous-object-type:error
+
+'use strict';
+
+import { Store as RelayModernStore, RecordSource, Environment as RelayModernEnvironment} from '../src';
+import { Network as RelayNetwork, createOperationDescriptor, createReaderSelector } from 'relay-runtime';
+import { createPersistedStorage } from './Utils';
+const { generateAndCompile } = require('relay-test-utils-internal');
+const RelayRecordSource = {
+  create: (data?: any) => new RecordSource({ storage: createPersistedStorage(), initialState: {...data}})
+}
+
+
+describe('applyUpdate()', () => {
+  let environment;
+  let operation;
+  let ParentQuery;
+  let source;
+  let store;
+  let UserFragment;
+
+  beforeEach(async () => {
+    jest.resetModules();
+
+    ({ParentQuery, UserFragment} = generateAndCompile(`
+        query ParentQuery {
+          me {
+            id
+            name
+          }
+        }
+        fragment UserFragment on User {
+          id
+          name
+        }
+      `));
+
+    source = RelayRecordSource.create();
+    store = new RelayModernStore(source);
+    environment = new RelayModernEnvironment({
+      network: RelayNetwork.create(jest.fn()),
+      store,
+    });
+    operation = createOperationDescriptor(ParentQuery, {});
+    await environment.hydrate();
+  });
+
+  it('applies the mutation to the store', () => {
+    const selector = createReaderSelector(
+      UserFragment,
+      '4',
+      {},
+      operation.request,
+    );
+    const callback = jest.fn();
+    const snapshot = environment.lookup(selector);
+    environment.subscribe(snapshot, callback);
+
+    environment.applyUpdate({
+      storeUpdater: proxyStore => {
+        const zuck = proxyStore.create('4', 'User');
+        zuck.setValue('4', 'id');
+        zuck.setValue('zuck', 'name');
+      },
+    });
+    expect(callback.mock.calls.length).toBe(1);
+    expect(callback.mock.calls[0][0].data).toEqual({
+      id: '4',
+      name: 'zuck',
+    });
+  });
+
+  it('reverts mutations when disposed', () => {
+    const selector = createReaderSelector(
+      UserFragment,
+      '4',
+      {},
+      operation.request,
+    );
+    const callback = jest.fn();
+    const snapshot = environment.lookup(selector);
+    environment.subscribe(snapshot, callback);
+
+    const {dispose} = environment.applyUpdate({
+      storeUpdater: proxyStore => {
+        const zuck = proxyStore.create('4', 'User');
+        zuck.setValue('zuck', 'name');
+      },
+    });
+    callback.mockClear();
+    dispose();
+    expect(callback.mock.calls.length).toBe(1);
+    expect(callback.mock.calls[0][0].data).toEqual(undefined);
+  });
+
+  it('can replace one mutation with another', () => {
+    const selector = createReaderSelector(
+      UserFragment,
+      '4',
+      {},
+      operation.request,
+    );
+    const callback = jest.fn();
+    const snapshot = environment.lookup(selector);
+    environment.subscribe(snapshot, callback);
+
+    callback.mockClear();
+    const updater = {
+      storeUpdater: proxyStore => {
+        const zuck = proxyStore.create('4', 'User');
+        zuck.setValue('4', 'id');
+      },
+    };
+    environment.applyUpdate(updater);
+    environment.replaceUpdate(updater, {
+      storeUpdater: proxyStore => {
+        const zuck = proxyStore.create('4', 'User');
+        zuck.setValue('4', 'id');
+        zuck.setValue('zuck', 'name');
+      },
+    });
+    expect(callback.mock.calls.length).toBe(2);
+    expect(callback.mock.calls[0][0].data).toEqual({
+      id: '4',
+    });
+    expect(callback.mock.calls[1][0].data).toEqual({
+      id: '4',
+      name: 'zuck',
+    });
+  });
+
+  it('notifies the subscription when an optimisitc update is reverted after commiting a server response for the same operation and also does not update the data subscribed', () => {
+    const selector = createReaderSelector(
+      UserFragment,
+      '4',
+      {},
+      operation.request,
+    );
+    const callback = jest.fn();
+    const snapshot = environment.lookup(selector);
+    expect(snapshot.data).toEqual(undefined);
+    environment.subscribe(snapshot, callback);
+
+    const disposable = environment.applyUpdate({
+      storeUpdater: proxyStore => {
+        const zuck = proxyStore.create('4', 'User');
+        zuck.setValue('4', 'id');
+        zuck.setValue('zuck', 'name');
+      },
+    });
+    expect(callback.mock.calls.length).toBe(1);
+    expect(callback.mock.calls[0][0].data).toEqual({
+      id: '4',
+      name: 'zuck',
+    });
+
+    callback.mockClear();
+    environment.commitPayload(operation, {
+      me: null,
+    });
+    expect(callback.mock.calls.length).toBe(0);
+
+    disposable.dispose();
+    expect(callback.mock.calls.length).toBe(1);
+    expect(callback.mock.calls[0][0].data).toEqual(undefined);
+
+    callback.mockClear();
+    const disposable2 = environment.applyUpdate({
+      storeUpdater: proxyStore => {
+        const zuck = proxyStore.get('4') ? proxyStore.get('4') : proxyStore.create('4', 'User');
+        zuck.setValue('4', 'id');
+        zuck.setValue('Mark', 'name');
+      },
+    });
+    expect(callback.mock.calls.length).toBe(1);
+    expect(callback.mock.calls[0][0].data).toEqual({
+      id: '4',
+      name: 'Mark',
+    });
+
+    callback.mockClear();
+    environment.commitPayload(operation, {
+      me: {
+        id: '4',
+        name: 'Zuck',
+      },
+    });
+    // no updates, overridden by still-applied optimistic update
+    expect(callback.mock.calls.length).toBe(0);
+
+    callback.mockClear();
+    disposable2.dispose();
+    expect(callback.mock.calls.length).toBe(1);
+    expect(callback.mock.calls[0][0].data).toEqual({
+      id: '4',
+      name: 'Zuck', // reverts to latest final value
+    });
+  });
+});
---
+++ packages/relay-offline/__tests__/RelayModernEnvironment-CheckWithGlobalInvalidation-test.ts
@@ -0,0 +1,568 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ * @emails oncall+relay
+ */
+
+// flowlint ambiguous-object-type:error
+
+'use strict';
+
+import { Store as RelayModernStore, RecordSource, Environment as RelayModernEnvironment} from '../src';
+import { Network as RelayNetwork, Observable as RelayObservable, createOperationDescriptor } from 'relay-runtime';
+import { createPersistedStorage } from './Utils';
+const { generateAndCompile } = require('relay-test-utils-internal');
+const RelayRecordSource = {
+  create: (data?: any) => new RecordSource({ storage: createPersistedStorage(), initialState: {...data}})
+}
+jest.useFakeTimers();
+
+describe('check() with global invalidation', () => {
+  let environment;
+  let operation;
+  let ParentQuery;
+  let source;
+  let store;
+  let complete;
+  let error;
+  let next;
+  let callbacks;
+  let dataSource;
+  let fetch;
+
+  beforeEach(async () => {
+    jest.resetModules();
+    ({ParentQuery} = generateAndCompile(`
+        query ParentQuery($size: [Int]!) {
+          me {
+            id
+            name
+            profilePicture(size: $size) {
+              uri
+            }
+          }
+        }
+      `));
+    operation = createOperationDescriptor(ParentQuery, {size: 32});
+
+    complete = jest.fn();
+    error = jest.fn();
+    next = jest.fn();
+    callbacks = {complete, error, next};
+    fetch = (_query, _variables, _cacheConfig) => {
+      return RelayObservable.create(sink => {
+        dataSource = sink;
+      });
+    };
+    source = RelayRecordSource.create();
+    store = new RelayModernStore(source, { defaultTTL: -1});
+    environment = new RelayModernEnvironment({
+      network: RelayNetwork.create(fetch),
+      store,
+    });
+    await environment.hydrate();
+    store.purge();
+  });
+
+  describe('when store is invalidated before query has ever been written to the store', () => {
+    it('returns available after receiving query from the server', () => {
+      environment.commitUpdate(storeProxy => {
+        storeProxy.invalidateStore();
+      });
+
+      environment.retain(operation);
+      environment.execute({operation}).subscribe(callbacks);
+      const payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+        },
+      };
+      const fetchTime = Date.now();
+      jest.spyOn(global.Date, 'now').mockImplementation(() => fetchTime);
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      expect(environment.check(operation)).toEqual({
+        status: 'available',
+        fetchTime,
+      });
+    });
+
+    it('returns missing if some data is missing after receiving query from the server', () => {
+      environment.commitUpdate(storeProxy => {
+        storeProxy.invalidateStore();
+      });
+
+      environment.retain(operation);
+      environment.execute({operation}).subscribe(callbacks);
+      const payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+        },
+      };
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      expect(environment.check(operation)).toEqual({status: 'missing'});
+    });
+  });
+
+  describe('when store is invalidated after query has been written to the store', () => {
+    it('returns stale even if full query is cached', () => {
+      environment.retain(operation);
+      environment.execute({operation}).subscribe(callbacks);
+      const payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+        },
+      };
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      environment.commitUpdate(storeProxy => {
+        storeProxy.invalidateStore();
+      });
+
+      // Should return stale even if all data is cached since
+      // store was invalidated after query completed
+      expect(environment.check(operation)).toEqual({status: 'stale'});
+    });
+
+    it('returns stale if some data is missing', () => {
+      environment.retain(operation);
+      environment.execute({operation}).subscribe(callbacks);
+      const payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+        },
+      };
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      environment.commitUpdate(storeProxy => {
+        storeProxy.invalidateStore();
+      });
+
+      expect(environment.check(operation)).toEqual({status: 'stale'});
+    });
+  });
+
+  describe('when query is refetched after store is invalidated', () => {
+    it('returns available if data is available after refetch', () => {
+      environment.retain(operation);
+      environment.execute({operation}).subscribe(callbacks);
+      let payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+        },
+      };
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      environment.commitUpdate(storeProxy => {
+        storeProxy.invalidateStore();
+      });
+
+      // Expect data to not be available after invalidation
+      expect(environment.check(operation)).toEqual({status: 'stale'});
+
+      environment.execute({operation}).subscribe(callbacks);
+      payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+        },
+      };
+      const fetchTime = Date.now();
+      jest.spyOn(global.Date, 'now').mockImplementation(() => fetchTime);
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      // Expect data be available after refetch
+      expect(environment.check(operation)).toEqual({
+        status: 'available',
+        fetchTime,
+      });
+    });
+
+    it('returns missing if data is not available after refetch', () => {
+      environment.retain(operation);
+      environment.execute({operation}).subscribe(callbacks);
+      let payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+        },
+      };
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      environment.commitUpdate(storeProxy => {
+        storeProxy.invalidateStore();
+      });
+
+      // Expect data to not be available after invalidation
+      expect(environment.check(operation)).toEqual({status: 'stale'});
+
+      environment.execute({operation}).subscribe(callbacks);
+      payload = {
+        data: {
+          me: {
+            __typename: 'User',
+            id: '4',
+            name: 'Zuck',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+        },
+      };
+      dataSource.next(payload);
+      dataSource.complete();
+      jest.runOnlyPendingTimers();
+
+      expect(environment.check(operation)).toEqual({status: 'missing'});
+    });
+  });
+
+  describe('when query has incremental payloads', () => {
+    beforeEach(() => {
+      ({ParentQuery} = generateAndCompile(`
+        query ParentQuery($size: [Int]!) {
+          me {
+            id
+            name
+            ...UserFragment @defer(label: "UserFragment")
+          }
+        }
+
+        fragment UserFragment on User {
+          profilePicture(size: $size) {
+            uri
+          }
+        }
+      `));
+      operation = createOperationDescriptor(ParentQuery, {size: 32});
+    });
+
+    describe('when store is invalidated before query has been written to the store', () => {
+      it('returns available after receiving payloads from the server', () => {
+        environment.commitUpdate(storeProxy => {
+          storeProxy.invalidateStore();
+        });
+
+        environment.retain(operation);
+        environment.execute({operation}).subscribe(callbacks);
+        const payload = {
+          data: {
+            me: {
+              __typename: 'User',
+              id: '4',
+              name: 'Zuck',
+            },
+          },
+        };
+        dataSource.next(payload);
+        jest.runOnlyPendingTimers();
+        next.mockClear();
+
+        // Still missing incremental payload
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+
+        const fetchTime = Date.now();
+        jest.spyOn(global.Date, 'now').mockImplementation(() => fetchTime);
+        dataSource.next({
+          data: {
+            id: '1',
+            __typename: 'User',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+          label: 'ParentQuery$defer$UserFragment',
+          path: ['me'],
+        });
+        dataSource.complete();
+        jest.runOnlyPendingTimers();
+
+        // Data for whole query should be available now
+        expect(environment.check(operation)).toEqual({
+          status: 'available',
+          fetchTime,
+        });
+      });
+
+      it('returns missing after receiving payloads from the server if data is still missing', () => {
+        environment.commitUpdate(storeProxy => {
+          storeProxy.invalidateStore();
+        });
+
+        environment.retain(operation);
+        environment.execute({operation}).subscribe(callbacks);
+        const payload = {
+          data: {
+            me: {
+              __typename: 'User',
+              id: '4',
+              name: 'Zuck',
+            },
+          },
+        };
+        dataSource.next(payload);
+        jest.runOnlyPendingTimers();
+        next.mockClear();
+
+        // Still missing incremental payload
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+
+        dataSource.next({
+          data: {
+            id: '1',
+            __typename: 'User',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+          label: 'ParentQuery$defer$UserFragment',
+          path: ['me'],
+        });
+        dataSource.complete();
+        jest.runOnlyPendingTimers();
+
+        // Data is still missing
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+      });
+    });
+
+    describe('when store is invalidated in between incremental payloads', () => {
+      it('returns stale after receiving payloads from the server', () => {
+        environment.retain(operation);
+        environment.execute({operation}).subscribe(callbacks);
+        const payload = {
+          data: {
+            me: {
+              __typename: 'User',
+              id: '4',
+              name: 'Zuck',
+            },
+          },
+        };
+        dataSource.next(payload);
+        jest.runOnlyPendingTimers();
+        next.mockClear();
+
+        // Still missing incremental payload
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+
+        // Invalidate the store in between incremental payloads
+        environment.commitUpdate(storeProxy => {
+          storeProxy.invalidateStore();
+        });
+
+        dataSource.next({
+          data: {
+            id: '1',
+            __typename: 'User',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+          label: 'ParentQuery$defer$UserFragment',
+          path: ['me'],
+        });
+        dataSource.complete();
+        jest.runOnlyPendingTimers();
+
+        // Should return false even if all data is cached since
+        // store was invalidated after first payload was written
+        expect(environment.check(operation)).toEqual({status: 'stale'});
+      });
+
+      it('returns stale after receiving payloads from the server and data is still missing', () => {
+        environment.retain(operation);
+        environment.execute({operation}).subscribe(callbacks);
+        const payload = {
+          data: {
+            me: {
+              __typename: 'User',
+              id: '4',
+              name: 'Zuck',
+            },
+          },
+        };
+        dataSource.next(payload);
+        jest.runOnlyPendingTimers();
+        next.mockClear();
+
+        // Still missing incremental payload
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+
+        // Invalidate the store in between incremental payloads
+        environment.commitUpdate(storeProxy => {
+          storeProxy.invalidateStore();
+        });
+
+        dataSource.next({
+          data: {
+            id: '1',
+            __typename: 'User',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+          label: 'ParentQuery$defer$UserFragment',
+          path: ['me'],
+        });
+        dataSource.complete();
+        jest.runOnlyPendingTimers();
+
+        expect(environment.check(operation)).toEqual({status: 'stale'});
+      });
+    });
+
+    describe('when store is invalidated after all incremental payloads have been written to the store', () => {
+      it('returns stale after receiving payloads from the server', () => {
+        environment.retain(operation);
+        environment.execute({operation}).subscribe(callbacks);
+        const payload = {
+          data: {
+            me: {
+              __typename: 'User',
+              id: '4',
+              name: 'Zuck',
+            },
+          },
+        };
+        dataSource.next(payload);
+        jest.runOnlyPendingTimers();
+        next.mockClear();
+
+        // Still missing incremental payload
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+
+        dataSource.next({
+          data: {
+            id: '1',
+            __typename: 'User',
+            profilePicture: {
+              uri: 'https://...',
+            },
+          },
+          label: 'ParentQuery$defer$UserFragment',
+          path: ['me'],
+        });
+        dataSource.complete();
+        jest.runOnlyPendingTimers();
+
+        // Invalidate the store after query has completed
+        environment.commitUpdate(storeProxy => {
+          storeProxy.invalidateStore();
+        });
+
+        // Should return stale even if all data is cached since
+        // store was invalidated after query completed
+        expect(environment.check(operation)).toEqual({status: 'stale'});
+      });
+
+      it('returns stale after receiving payloads from the server and data is still missing', () => {
+        environment.retain(operation);
+        environment.execute({operation}).subscribe(callbacks);
+        const payload = {
+          data: {
+            me: {
+              __typename: 'User',
+              id: '4',
+              name: 'Zuck',
+            },
+          },
+        };
+        dataSource.next(payload);
+        jest.runOnlyPendingTimers();
+        next.mockClear();
+
+        // Still missing incremental payload
+        expect(environment.check(operation)).toEqual({status: 'missing'});
+
+        dataSource.next({
+          data: {
+            id: '1',
+            __typename: 'User',
+            profilePicture: {
+              uri: undefined,
+            },
+          },
+          label: 'ParentQuery$defer$UserFragment',
+          path: ['me'],
+        });
+        dataSource.complete();
+        jest.runOnlyPendingTimers();
+
+        // Invalidate the store after query has completed
+        environment.commitUpdate(storeProxy => {
+          storeProxy.invalidateStore();
+        });
+
+        expect(environment.check(operation)).toEqual({status: 'stale'});
+      });
+    });
+  });
+});
